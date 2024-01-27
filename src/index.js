a88cecd90aa7c17509d83e7414040c8f62a835ba
Alan Rocha <141082245+alanrochagomes@users.noreply.github.com>
2024-01-27 10:09:02 -0300

    Arquivos da pasta src removidos e estrutura básica com estilização adicionada

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function Board() {
+  return (
+    <div>
+      <div className='board-row'>
+        <button className='square'></button>
+        <button className='square'></button>
+        <button className='square'></button>
+      </div>
+      
+      <div className='board-row'>
+        <button className='square'></button>
+        <button className='square'></button>
+        <button className='square'></button>
+      </div>
+      
+      <div className='board-row'>
+        <button className='square'></button>
+        <button className='square'></button>
+        <button className='square'></button>
+      </div>
+    </div>
+  )
+}
+
+function Game() {
+  return (
+    <div class='game'>
+      <div className='game-board'>
+        <Board/>
+      </div>
+
+      <div className='game-info'>
+        Game info
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <Game/>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
